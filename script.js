b190b185c9cb65dc5ef4486826c6a6870db3782d
fypsoluciones <fypasesoriaintegral@gmail.com>
2026-06-28 19:03:07 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// ================================
+// FyP - Consultoría Contable y Empresarial
+// Archivo JavaScript
+// ================================
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Sitio FyP cargado correctamente.");
+});
